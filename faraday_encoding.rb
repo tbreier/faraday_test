159e9d30e460d52424407bb6c63620a14e5093f7
Thomas Breier <breierthomas@gmail.com>
2015-03-23 18:25:34 -0700

    add working solution

--- faraday_encoding.rb
+++ faraday_encoding.rb
@@ -1,4 +1,5 @@
 require 'faraday'
+require 'uri'
 
 connection = Faraday.new do |c|
   c.request :url_encoded
@@ -7,7 +8,7 @@ connection = Faraday.new do |c|
 end
 
 connection.post do |request|
-  request.url 'http://localhost:3000/' # returns 200 Success when I use my Rails server
+  request.url 'http://localhost:3000/' # returns 200 OK when I use my Rails server
   request.body = "From: \"Me\" <info@info.com>\nTo: \"You\" <info@info.com>\nHello"
 end
 
@@ -16,4 +17,11 @@ puts "----------------------------------------------"
 connection.post do |request|
   request.url 'http://localhost:3000/' # returns 400 Bad Request when I use my Rails server
   request.body = "From: \"Me\" <info@info.com>\nTo: \"You\" <info@info.com>\nHello %%"
+end
+
+puts "----------------------------------------------"
+
+connection.post do |request|
+  request.url 'http://localhost:3000/' # returns 200 OK when I use my Rails server
+  request.body = URI.encode_www_form_component("From: \"Me\" <info@info.com>\nTo: \"You\" <info@info.com>\nHello %%")
 end
