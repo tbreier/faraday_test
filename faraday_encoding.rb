4b5c0e2351a22183c478353ba51489b5bfdd6bf5
Thomas Breier <breierthomas@gmail.com>
2015-03-23 17:42:48 -0700

    not needed

--- faraday_encoding.rb
+++ faraday_encoding.rb
@@ -5,8 +5,6 @@ connection = Faraday.new do |c|
   c.adapter Faraday.default_adapter
 end
 
-connection.token_auth 'my_token'
-
 connection.post do |request|
   request.url 'http://localhost:3000/' # returns 200 Success when I use my Rails server
   request.body = "From: \"Me\" <info@info.com>\nTo: \"You\" <info@info.com>\nHello"
